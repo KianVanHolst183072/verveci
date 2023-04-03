162d95aa7b9f933e1fa6162a7db6785ed40bff33
Kian <183072@buas.nl>
2023-04-03 13:05:18 +0200

    update

--- benchmark.py
+++ benchmark.py
@@ -1,59 +1,75 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[11]:
+# In[59]:
 
 
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 
-# In[5]:
+# In[60]:
 
 
 df = pd.read_csv("titanic.csv")
 
 
-# In[18]:
+# In[62]:
 
 
-st.title("Benchmark")
+del df['Cabin']
 
 
-# In[21]:
+# In[63]:
 
 
-section_1 = (
-st.header("Competency 1"),
-st.markdown("This will be the content of section 1")
-)
+df = df.dropna()
 
 
-# In[19]:
+# In[64]:
 
 
-section_2 = (
-st.header("Competency 2"),
-st.markdown("This will be the content of section 2")
-)
+df['Age'] = df['Age'].astype(int)
 
 
-# In[22]:
+# In[65]:
 
 
-current_page = section_1
+st.title("Results")
 
 
-# In[23]:
+# In[66]:
 
 
-current_page
+user_input = df.iloc[10]
 
 
-# In[25]:
+# In[69]:
 
 
-if st.button('Next page'):
-    current_page = section_2
-    current_page
+import streamlit as st
+
+tab1, tab2, tab3 = st.tabs(["Demographics", "Dog", "Owl"])
+
+with tab1:
+   st.header("Demographics")
+   with st.container():
+      col1, col2, col3 = st.columns([2,3,1])
+      with col1:
+         st.subheader("Passenger name:", user_input[3])
+         st.subheader("Passenger age:", str(user_input[5]))
+         st.subheader(f"Passenger sex: {user_input[1]}")
+      with col2:
+         st.text("Data")
+      with col3:
+         st.text("Text")
+
+
+with tab2:
+   st.header("Competency 2")
+
+
+with tab3:
+   st.header("An owl")
 
